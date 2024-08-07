74c9ed902f142a87c03bc978798f463ec910c269
Abstract <xavier04.martinez@gmail.com>
2024-08-07 16:20:55 -0600

    Update middleware.go

--- common/middleware/middleware.go
+++ common/middleware/middleware.go
@@ -1,22 +1,22 @@
 package middleware
 
 import (
-    "github.com/holypvp/primal/common"
-    "github.com/labstack/echo/v4"
-    "net/http"
+	"github.com/holypvp/primal/common"
+	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func HandleBasicAuth(next echo.HandlerFunc) echo.HandlerFunc {
-    return func(c echo.Context) error {
-        apiKey := c.Request().Header.Get("X-API-Key")
-        if apiKey == common.APIKey {
-            return next(c)
-        }
+	return func(c echo.Context) error {
+		apiKey := c.Request().Header.Get("X-API-Key")
+		if apiKey == common.APIKey {
+			return next(c)
+		}
 
-        if apiKey == "" {
-            return echo.NewHTTPError(http.StatusUnauthorized, "API key is required")
-        }
+		if apiKey == "" {
+			return echo.NewHTTPError(http.StatusUnauthorized, "API key is required")
+		}
 
-        return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
-    }
+		return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
+	}
 }
