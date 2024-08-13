8cb8004bb2665fdb740414b9c6e3cf49d291248b
Abstract <xavier04.martinez@gmail.com>
2024-08-13 16:31:57 -0600

    refactor(server): object renamed to model

--- server/routes/server_down_route.go
+++ server/routes/server_down_route.go
@@ -1,37 +1,37 @@
 package routes
 
 import (
-    "context"
-    "fmt"
-    "github.com/holypvp/primal/common"
-    "github.com/holypvp/primal/server"
-    "github.com/holypvp/primal/server/pubsub"
-    "github.com/labstack/echo/v4"
-    "net/http"
+	"context"
+	"fmt"
+	"github.com/holypvp/primal/common"
+	"github.com/holypvp/primal/server"
+	"github.com/holypvp/primal/server/pubsub"
+	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func ServerDownRoute(c echo.Context) error {
-    serverId := c.Param("id")
-    if serverId == "" {
-        return echo.NewHTTPError(http.StatusBadRequest, "No ID found")
-    }
+	serverId := c.Param("id")
+	if serverId == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "No ID found")
+	}
 
-    i := server.Service().LookupById(serverId)
-    if i == nil {
-        return echo.NewHTTPError(http.StatusNoContent, fmt.Sprintf("Server %s not found", serverId))
-    }
+	i := server.Service().LookupById(serverId)
+	if i == nil {
+		return echo.NewHTTPError(http.StatusNoContent, fmt.Sprintf("Server %s not found", serverId))
+	}
 
-    go func() {
-        payload, err := common.WrapPayload("API_SERVER_DOWN", pubsub.NewServerStatusPacket(i.Id()))
-        if err != nil {
-            common.Log.Fatal("Failed to marshal packet: ", err)
-        }
+	go func() {
+		payload, err := common.WrapPayload("API_SERVER_DOWN", pubsub.NewServerStatusPacket(i.Id()))
+		if err != nil {
+			common.Log.Fatal("Failed to marshal packet: ", err)
+		}
 
-        err = common.RedisClient.Publish(context.Background(), common.RedisChannel, payload).Err()
-        if err != nil {
-            common.Log.Errorf("Failed to publish packet: %v", err)
-        }
-    }()
+		err = common.RedisClient.Publish(context.Background(), common.RedisChannel, payload).Err()
+		if err != nil {
+			common.Log.Errorf("Failed to publish packet: %v", err)
+		}
+	}()
 
-    return c.String(http.StatusOK, fmt.Sprintf("Server %s is now down", i.Id()))
+	return c.String(http.StatusOK, fmt.Sprintf("Server %s is now down", i.Id()))
 }
